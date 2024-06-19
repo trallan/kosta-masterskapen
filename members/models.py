ec1fa44800eb6506ec36139d8bf2784995371cb1
trallan <vicke_erlandsson@hotmail.com>
2024-06-19 21:52:15 +0200

    first commit

---
+++ members/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Member(models.Model):
+  firstname = models.CharField(max_length=100)
+  lastname = models.CharField(max_length=100)
+  golf_club = models.CharField(default="", max_length=100)
+  golf_id = models.CharField(max_length=10, null=True)
+  hcp = models.FloatField(null=True)
+  wins = models.PositiveSmallIntegerField(default=0)
+  phone = models.IntegerField(null=True)
+  joined_date = models.DateField(null=True)
+  slug = models.SlugField(default="", null=False)
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
